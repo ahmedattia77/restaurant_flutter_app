5cd7d44f7dcb3052e5c9795387f098e08759b1e7
ahmedattia77 <max.kali.1212@gmail.com>
2026-06-21 09:32:49 +0300

    modification account page

--- lib/views/account/pages/account_page.dart
+++ lib/views/account/pages/account_page.dart
@@ -17,111 +17,112 @@ class AccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 60),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              ProfilePicture(),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, bottom: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        ProfileHeadName(),
-                        SizedBox(width: 5),
-                        ProfileNameEditorIcon(),
-                        SizedBox(width: 40),
-                        ValueListenableBuilder(
-                          valueListenable: isDark,
-                          builder: (context, value, child) {
-                            return IconButton(
-                              iconSize: 28,
-                              icon: Icon(
-                                value
-                                    ? Icons.dark_mode_outlined
-                                    : Icons.light_mode_outlined,
-                              ),
-                              onPressed: () async {
-                                final SharedPreferences prefs =
-                                    await SharedPreferences.getInstance();
-                                isDark.value = !value;
-                                prefs.setBool(isDarkMode, isDark.value);
-                              },
-                            );
-                          },
-                        ),
-                      ],
+      child: Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const ProfilePicture(),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, bottom: 5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const ProfileHeadName(),
+                          const SizedBox(width: 5),
+                          const ProfileNameEditorIcon(),
+                          const SizedBox(width: 40),
+                          ValueListenableBuilder(
+                            valueListenable: isDark,
+                            builder: (context, value, child) {
+                              return IconButton(
+                                iconSize: 28,
+                                icon: Icon(
+                                  value
+                                      ? Icons.dark_mode_outlined
+                                      : Icons.light_mode_outlined,
+                                ),
+                                onPressed: () async {
+                                  final SharedPreferences prefs =
+                                      await SharedPreferences.getInstance();
+                                  isDark.value = !value;
+                                  prefs.setBool(isDarkMode, isDark.value);
+                                },
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      const ProfileHeadEmail(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+        
+            const SizedBox(height: 30),
+        
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: accountSettingsData.length,
+                itemBuilder: (context, index) {
+                  final item = accountSettingsData[index];
+                  return AccountSettingsItem(icon: item.icon, title: item.title);
+                },
+              ),
+            ),
+        
+            Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              child: ElevatedButton(
+                onPressed: () async {
+                  final SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                    await prefs.remove(isLoggedInKey);
+        
+                  Navigator.pushAndRemoveUntil(context,
+                    MaterialPageRoute(builder: (context) => const Onboarding()),
+                    (route) => false,
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffF2F3F2),
+                  elevation: 3,
+                  minimumSize: const Size(double.infinity, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(19),
+                  ),
+                ),
+        
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Icon(
+                      Icons.logout_outlined,
+                      color: Color(0xff53B175),
+                      size: 24,
                     ),
-                    ProfileHeadEmail(),
+                    SizedBox(width: 75),
+                    Text(
+                      'Log Out',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff53B175),
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
-
-          SizedBox(height: 30),
-
-          Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: accountSettingsData.length,
-              itemBuilder: (context, index) {
-                final item = accountSettingsData[index];
-                return AccountSettingsItem(icon: item.icon, title: item.title);
-              },
             ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 20, bottom: 20),
-            child: ElevatedButton(
-              onPressed: () async {
-                final SharedPreferences prefs =
-                    await SharedPreferences.getInstance();
-                  await prefs.remove(isLoggedInKey);
-
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Onboarding()),
-                  (route) => false,
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xffF2F3F2),
-                elevation: 3,
-                minimumSize: const Size(double.infinity, 60),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(19),
-                ),
-              ),
-
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  Icon(
-                    Icons.logout_outlined,
-                    color: Color(0xff53B175),
-                    size: 24,
-                  ),
-                  SizedBox(width: 75),
-                  Text(
-                    'Log Out',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff53B175),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/views/account/widgets/profile_picture.dart
+++ lib/views/account/widgets/profile_picture.dart
@@ -6,10 +6,24 @@ class ProfilePicture extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 70,
+      height: 70,
       decoration: BoxDecoration(shape: BoxShape.circle),
-      child: CircleAvatar(
-        radius: 35,
-        backgroundImage: AssetImage('assets/images/ironman.jpg', ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(35),
+
+        child: Image.asset(
+          'assets/images/ironman.jpg',
+          frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+            if (wasSynchronouslyLoaded) return child;
+            return AnimatedOpacity(
+              opacity: frame == null ? 0 : 1,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+              child: child,
+            );
+          },
+        ),
       ),
     );
   }
